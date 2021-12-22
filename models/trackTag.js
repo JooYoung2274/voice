a02b3806d54c8872af3e64074d814bea469938ee
JUYOUNG KIM <>
2021-12-22 13:42:18 +0900

    DB schema update / track, trackTag

---
+++ models/trackTag.js
@@ -0,0 +1,47 @@
+"use strict";
+const Sequelize = require("sequelize");
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class TrackTag extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      TrackTag.belongsTo(models.Track, { foreignKey: "trackId" });
+      TrackTag.belongsTo(models.Tag, { foreignKey: "tagId" });
+    }
+  }
+  TrackTag.init(
+    {
+      trackTagId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      tagId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      trackId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    },
+    {
+      sequelize,
+      modelName: "TrackTag",
+    },
+  );
+  return TrackTag;
+};
