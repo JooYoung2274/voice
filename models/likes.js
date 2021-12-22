2cdb15e19bce85240611b5e60a37a9f52afa37fd
golddong98 <73287240+golddong98@users.noreply.github.com>
2021-12-23 03:48:51 +0900

    like api 완료 연결test필요

--- models/likes.js
+++ models/likes.js
@@ -2,28 +2,24 @@
 const Sequelize = require("sequelize");
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Comments extends Model {
+  class Likes extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Comments.belongsTo(models.Users, { foreignKey: "userId" });
-      Comments.belongsTo(models.Tracks, { foreignKey: "trackId" });
+      Likes.belongsTo(models.Users, { foreignKey: "userId" });
+      Likes.belongsTo(models.Tracks, { foreignKey: "trackId" });
     }
   }
-  Comments.init(
+  Likes.init(
     {
-      commentId: {
+      likeId: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER,
-      },
-      comment: {
-        allowNull: false,
-        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
@@ -44,8 +40,8 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       sequelize,
-      modelName: "Comments",
+      modelName: "Likes",
     },
   );
-  return Comments;
+  return Likes;
 };
--- router/index.js
+++ router/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const commentRouter = require("./comments.js");
+const likeRouter = require("./likes.js");
 const router = express.Router();
 
-// router.use("/tracks");
-router.use("/", commentRouter);
+router.use("/tracks", commentRouter);
+router.use("/tracks", likeRouter);
 
 module.exports = router;
