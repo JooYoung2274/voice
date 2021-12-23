06a09929223e291459f787c70117052069b8cf8d
doyeon8621 <59997108+doyeon8621@users.noreply.github.com>
2021-12-23 10:01:08 +0900

    commit auth

---
+++ middleware/auth-middleware.js
@@ -0,0 +1,46 @@
+const jwt = require("jsonwebtoken");
+const { Users } = require("../models");
+//로그인 필수
+const needLogin = (req, res, next) => {
+  const { authorization } = req.headers;
+  const [tokenType, tokenValue] = authorization.split(" ");
+  console.log(authorization);
+  if (tokenType !== "Bearer") {
+    res.status(401).send({
+      errorMessage: "로그인 후 사용하세요.",
+    });
+    return;
+  }
+
+  try {
+    const { userId } = jwt.verify(tokenValue, "secret-secret-key");
+    Users.findOne({ where: { userId: userId } }).then((user) => {
+      // async가 없으므로 await은 안됨. sequelize는 기본적으로 promise이므로  then
+      res.locals.user = user;
+      next();
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(401).send({
+      errorMessage: "로그인 후 사용하세요.",
+    });
+    return;
+  }
+};
+
+//로그인 불필요
+const notNeedLogin = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    const [tokenType, tokenValue] = authorization.split(" ");
+
+    const { userId } = jwt.verify(tokenValue, "secret-secret-key");
+    User.findByPk(userId).then((user) => {
+      // async가 없으므로 await은 안됨. sequelize는 기본적으로 promise이므로  then
+      res.locals.user = user;
+    });
+  }
+  next();
+};
+
+module.exports = { needLogin, notNeedLogin };
