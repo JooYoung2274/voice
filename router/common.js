32d6ae4e46544b73e03d45052969ed389dab81db
JUYOUNG KIM <>
2021-12-23 09:11:31 +0900

    router update / common, track

--- router/common.js
+++ router/common.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const commonController = require("../controller/commonController");
 
-router.get("/", commonController.mainPageTracksGet);
-router.get("/user/:userId", commonController.myPagetTracksGet);
+router.get("/", commonController.mainTracksGet);
+router.get("/user/:userId", commonController.myTracksGet);
 router.get("/track/:trackId", commonController.detailTrackGet);
+
+router.get("/search", commonController.categorySelect);
 
 module.exports = router;
--- router/track.js
+++ router/track.js
@@ -3,12 +3,10 @@ const router = express.Router();
 const trackController = require("../controller/trackController");
 
 const uploader = require("../middleware/uploader");
-const imageUploader = uploader.fields([
-  { name: "thumbnailUrl" },
-  { name: "trackUrl" },
-]);
+const imageUploader = uploader.fields([{ name: "thumbnailUrl" }, { name: "trackUrl" }]);
 
 router.post("/", imageUploader, trackController.trackUploads);
+router.get("/:trackId", trackController.trackPage);
 // router.delete("/:trackId", trackController.trackDelete);
 
 module.exports = router;
