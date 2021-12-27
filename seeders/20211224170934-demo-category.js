432a7288e34eaccc70d8e2cbcc011f4b0040aa6b
golddong98 <73287240+golddong98@users.noreply.github.com>
2021-12-27 09:45:41 +0900

    seeders수정 like comment수정

--- seeders/20211224170934-demo-category.js
+++ seeders/20211224170934-demo-category.js
@@ -7,22 +7,25 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert("Categories", [
       {
+        category: "전체",
+      },
+      {
+        category: "일상 언어",
+      },
+      {
+        category: "ASMR",
+      },
+      {
+        category: "연기",
+      },
+      {
+        category: "노래",
+      },
+      {
         category: "나레이션",
       },
       {
-        category: "라디오",
-      },
-      {
-        category: "오디오북",
-      },
-      {
-        category: "더빙",
-      },
-      {
         category: "효과음",
-      },
-      {
-        category: "TV",
       },
       {
         category: "성대모사",
