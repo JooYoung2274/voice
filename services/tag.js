45c8124a6701f2f28a6d8b18c0449eb1df769695
JUYOUNG KIM <wndud2274@gmail.com>
2021-12-27 21:19:46 +0900

    [UPDATE] models/category, categoryUrl 추가

--- services/tag.js
+++ services/tag.js
@@ -1,44 +1,7 @@
-const { Tag, TrackTag } = require("../models");
+const { TrackTag } = require("../models");
 
 const sequelize = require("sequelize");
 const Op = sequelize.Op;
-
-const getTagIdByTag = async ({ tag }) => {
-  let result = [];
-  for (let i = 0; i < 3; i++) {
-    const findedTag = await Tag.findOne({
-      attributes: ["tagId"],
-      where: { tag: tag[i] },
-    });
-    if (!findedTag) {
-      continue;
-    }
-    result.push(findedTag.tagId);
-  }
-  if (!tagId) {
-    return;
-  }
-  return result;
-};
-
-const getTagIdsByTags = async ({ tag1, tag2, tag3 }) => {
-  const findedTag = await Tag.findAll({
-    attributes: ["tagId"],
-    where: {
-      tag: {
-        [Op.or]: [tag1, tag2, tag3],
-      },
-    },
-  });
-  if (!findedTag) {
-    return;
-  }
-  let result = [];
-  for (let i = 0; i < findedTag.length; i++) {
-    result.push(findedTag[i].tagId);
-  }
-  return result;
-};
 
 const getTrackIdsByTag = async ({ tag, category }) => {
   const findedTracks = await TrackTag.findAll({
