ec11b2cf91479b2d73c548eacc06fefcb8da73a2
golddong98 <73287240+golddong98@users.noreply.github.com>
2021-12-27 22:09:09 +0900

    [CREATE] search api완성

--- services/search.js
+++ services/search.js
@@ -1,55 +1,29 @@
-const { Track, Comment, Users, TrackTag } = require("../models");
+const { Track, Users, TrackTag } = require("../models");
 const { Op } = require("sequelize");
 const { or, like } = Op;
 
-// //keyword로 track 가져오는 서비스함수
-// const getTracksByKeywordTEST = async ({ keyword }) => {
-//   try {
-//     //   공백이 2개이상 존재하면 하나의 공백으로 변환(아직 더 생각해봐야함)
-//     // keyword = keyword.replace(/\s\s+/gi, " ");
-
-//     const trackSearchCondition = [
-//       { title: { [like]: `%${keyword}%` } },
-//       {
-//         "$User.nickname$": { [like]: `%${keyword}%` },
-//       },
-//     ];
-
-//     const results = await Track.findAll({
-//       where: {
-//         [or]: trackSearchCondition,
-//       },
-//       attributes: ["trackId", "category", "trackThumbnailUrl", "trackUrl", "userId"],
-//       include: [
-//         { model: TrackTag, attributes: ["tag"] },
-//         { model: Users, as: "User", attributes: ["nickname"] },
-//       ],
-//     });
-
-//     return results;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
-
-//keyword로 comments가져오는 함수
+//keyword로 track 가져오는 서비스함수
 const getTracksByKeyword = async ({ keyword }) => {
   try {
+    //   공백이 2개이상 존재하면 하나의 공백으로 변환(아직 더 생각해봐야함)
     // keyword = keyword.replace(/\s\s+/gi, " ");
 
-    const results = await Comment.findAll({
-      where: {
-        [or]: [
-          {
-            comment: { [like]: `%${keyword}%` },
-          },
-          {
-            "$User.nickname$": { [like]: `%${keyword}%` },
-          },
-        ],
+    const trackSearchCondition = [
+      { title: { [like]: `%${keyword}%` } },
+      {
+        "$User.nickname$": { [like]: `%${keyword}%` },
       },
-      attributes: ["commentId", "createdAt"],
-      include: [{ model: Users, as: "User", attributes: ["nickname"] }],
+    ];
+
+    const results = await Track.findAll({
+      where: {
+        [or]: trackSearchCondition,
+      },
+      attributes: ["trackId", "category", "trackThumbnailUrl", "trackUrl", "userId"],
+      include: [
+        { model: TrackTag, attributes: ["tag"] },
+        { model: Users, as: "User", attributes: ["nickname"] },
+      ],
     });
 
     return results;
@@ -57,5 +31,31 @@ const getTracksByKeyword = async ({ keyword }) => {
     throw error;
   }
 };
+
+// //keyword로 comments가져오는 함수
+// const getTracksByKeyword = async ({ keyword }) => {
+//   try {
+//     // keyword = keyword.replace(/\s\s+/gi, " ");
+
+//     const results = await Comment.findAll({
+//       where: {
+//         [or]: [
+//           {
+//             comment: { [like]: `%${keyword}%` },
+//           },
+//           {
+//             "$User.nickname$": { [like]: `%${keyword}%` },
+//           },
+//         ],
+//       },
+//       attributes: ["commentId", "createdAt"],
+//       include: [{ model: Users, as: "User", attributes: ["nickname"] }],
+//     });
+
+//     return results;
+//   } catch (error) {
+//     throw error;
+//   }
+// };
 
 module.exports = { getTracksByKeyword };
