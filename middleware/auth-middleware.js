9eac7a635ae381377305ae2312a67da2116be3cd
doyeon8621 <59997108+doyeon8621@users.noreply.github.com>
2021-12-29 11:38:58 +0900

    [FIX]auth미들웨어 에러수정

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -1,26 +1,31 @@
 const jwt = require("jsonwebtoken");
 const { User } = require("../models");
+const { customizedError } = require("../utils/error");
 //로그인 필수
 const needLogin = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization) {
-    throw customizedError("토큰이 유효하지 않습니다.", 401);
-  }
-  const [tokenType, tokenValue] = authorization.split(" ");
-  if (tokenType !== "Bearer") {
-    throw customizedError("토큰이 유효하지 않습니다.", 401);
-  }
-
   try {
-    const { userId } = jwt.verify(tokenValue, process.env.JWT_SECRET);
-    console.log(userId + "????????");
+    const { authorization } = req.headers;
+    if (!authorization) {
+      throw customizedError("토큰이 유효하지 않습니다.", 401);
+    }
+    const [tokenType, tokenValue] = authorization.split(" ");
+    if (tokenType !== "Bearer") {
+      throw customizedError("토큰이 유효하지 않습니다.", 401);
+    }
+
+    const result = jwt.verify(tokenValue, process.env.JWT_SECRET, (error, decoded) => {
+      if (error) {
+        throw customizedError("토큰이 유효하지 않습니다.", 401);
+      }
+      return decoded;
+    });
+    const { userId } = result;
     User.findOne({ where: { userId: userId } }).then((user) => {
       // async가 없으므로 await은 안됨. sequelize는 기본적으로 promise이므로  then
       res.locals.user = user;
       next();
     });
   } catch (error) {
-    console.log(error);
     next(error);
   }
 };
