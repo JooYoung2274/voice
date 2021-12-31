14561d035f1de7b1c7297b0e9f263cdcc0e832f8
JUYOUNG KIM <wndud2274@gmail.com>
2021-12-31 15:09:57 +0900

    [update] seeders 파일 컬럼 수정

--- seeders/20211226145427-demo-trackThumbnail.js
+++ seeders/20211226145427-demo-trackThumbnail.js
@@ -4,40 +4,40 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert("TrackThumbnails", [
       {
-        trackThumbnailUrlFace: "http://54.180.82.210/trackThumbnail/OAO1_face.png",
-        trackThumbnailUrlFull: "http://54.180.82.210/trackThumbnail/OAO1_full.png",
+        trackThumbnailUrlFace: "http://13.125.215.6/trackThumbnail/OAO1_face.png",
+        trackThumbnailUrlFull: "http://13.125.215.6/trackThumbnail/OAO1_full.png",
       },
       {
-        trackThumbnailUrlFace: "http://54.180.82.210/trackThumbnail/OAO2_face.png",
-        trackThumbnailUrlFull: "http://54.180.82.210/trackThumbnail/OAO2_full.png",
+        trackThumbnailUrlFace: "http://13.125.215.6/trackThumbnail/OAO2_face.png",
+        trackThumbnailUrlFull: "http://13.125.215.6/trackThumbnail/OAO2_full.png",
       },
       {
-        trackThumbnailUrlFace: "http://54.180.82.210/trackThumbnail/OAO3_face.png",
-        trackThumbnailUrlFull: "http://54.180.82.210/trackThumbnail/OAO3_full.png",
+        trackThumbnailUrlFace: "http://13.125.215.6/trackThumbnail/OAO3_face.png",
+        trackThumbnailUrlFull: "http://13.125.215.6/trackThumbnail/OAO3_full.png",
       },
       {
-        trackThumbnailUrlFace: "http://54.180.82.210/trackThumbnail/OAO4_face.png",
-        trackThumbnailUrlFull: "http://54.180.82.210/trackThumbnail/OAO4_full.png",
+        trackThumbnailUrlFace: "http://13.125.215.6/trackThumbnail/OAO4_face.png",
+        trackThumbnailUrlFull: "http://13.125.215.6/trackThumbnail/OAO4_full.png",
       },
       {
-        trackThumbnailUrlFace: "http://54.180.82.210/trackThumbnail/OAO5_face.png",
-        trackThumbnailUrlFull: "http://54.180.82.210/trackThumbnail/OAO5_full.png",
+        trackThumbnailUrlFace: "http://13.125.215.6/trackThumbnail/OAO5_face.png",
+        trackThumbnailUrlFull: "http://13.125.215.6/trackThumbnail/OAO5_full.png",
       },
       {
-        trackThumbnailUrlFace: "http://54.180.82.210/trackThumbnail/OAO6_face.png",
-        trackThumbnailUrlFull: "http://54.180.82.210/trackThumbnail/OAO6_full.png",
+        trackThumbnailUrlFace: "http://13.125.215.6/trackThumbnail/OAO6_face.png",
+        trackThumbnailUrlFull: "http://13.125.215.6/trackThumbnail/OAO6_full.png",
       },
       {
-        trackThumbnailUrlFace: "http://54.180.82.210/trackThumbnail/OAO7_face.png",
-        trackThumbnailUrlFull: "http://54.180.82.210/trackThumbnail/OAO7_full.png",
+        trackThumbnailUrlFace: "http://13.125.215.6/trackThumbnail/OAO7_face.png",
+        trackThumbnailUrlFull: "http://13.125.215.6/trackThumbnail/OAO7_full.png",
       },
       {
-        trackThumbnailUrlFace: "http://54.180.82.210/trackThumbnail/OAO8_face.png",
-        trackThumbnailUrlFull: "http://54.180.82.210/trackThumbnail/OAO8_full.png",
+        trackThumbnailUrlFace: "http://13.125.215.6/trackThumbnail/OAO8_face.png",
+        trackThumbnailUrlFull: "http://13.125.215.6/trackThumbnail/OAO8_full.png",
       },
       {
-        trackThumbnailUrlFace: "http://54.180.82.210/trackThumbnail/OAO9_face.png",
-        trackThumbnailUrlFull: "http://54.180.82.210/trackThumbnail/OAO9_full.png",
+        trackThumbnailUrlFace: "http://13.125.215.6/trackThumbnail/OAO9_face.png",
+        trackThumbnailUrlFull: "http://13.125.215.6/trackThumbnail/OAO9_full.png",
       },
     ]);
   },
