717424effb2d25483911ac5217bbe09ccde63a46
JUYOUNG KIM <wndud2274@gmail.com>
2022-01-06 18:04:44 +0900

    test29

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = require("./app.js");
 const { SERVER_PORT } = process.env;
 
 const server = http.createServer(app);
-//tst
+//아니 그게 무슨 소리야 왜 할게없다고 그래
 connectDB();
 server.listen(SERVER_PORT, () => {
   console.log(`listening at http://localhost:${SERVER_PORT}`);
