10eb2a05f3c855f2aaa3d8f24557f01dcdc54bc5
JUYOUNG KIM <wndud2274@gmail.com>
2022-01-06 17:26:41 +0900

    test2

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const { SERVER_PORT } = process.env;
 
 const server = http.createServer(app);
 
-// const test = "tesdf33dsfadfd
-
 connectDB();
 server.listen(SERVER_PORT, () => {
   console.log(`listening at http://localhost:${SERVER_PORT}`);
