8a94ae357de5110b4b5f4f3f34ebb1d4c6bd3276
doyeon8621 <59997108+doyeon8621@users.noreply.github.com>
2022-01-11 22:03:12 +0900

    [update]랭킹이미지 추가

--- services/statistics.js
+++ services/statistics.js
@@ -1,5 +1,7 @@
 const { Track, Like, Category, TrackTag, Tag, User } = require("../models");
 const { Op } = require("sequelize");
+const { S3_HOST } = process.env;
+const DIRECTORY = "etc";
 
 //해당하는 모든 trackId 찾기
 const TrackIdsByUserId = async (userId) => {
@@ -159,20 +161,26 @@ const getRanks = async () => {
     const rank = i + 1;
     const rate = rank / parents;
     let classes = "";
+    let classImage = "";
     if (rate > 0 && rate <= 0.2) {
       classes = "탑스타 와오";
+      classImage = `${S3_HOST}/${DIRECTORY}/OAO_5단계.png`;
     } else if (rate <= 0.4) {
       classes = "핫한 와오";
+      classImage = `${S3_HOST}/${DIRECTORY}/OAO_4단계.png`;
     } else if (rate <= 0.6) {
       classes = "라이징스타 와오";
+      classImage = `${S3_HOST}/${DIRECTORY}/OAO_3단계.png`;
     } else if (rate <= 0.8) {
       classes = "끼쟁이 와오";
+      classImage = `${S3_HOST}/${DIRECTORY}/OAO_2단계.png`;
     } else {
       classes = "수줍은 와오";
+      classImage = `${S3_HOST}/${DIRECTORY}/OAO_1단계.png`;
     }
-    return { rank: rank, class: classes, ...el };
+    return { rank: rank, class: classes, classImage: classImage, ...el };
   });
-  //console.log(LikesCountRank);
+  console.log(LikesCountRank);
   return LikesCountRank;
 };
 //user하나의 순위와 등급
@@ -184,6 +192,7 @@ const getRankByUserId = async ({ userId }) => {
       let rankByUserId = {};
       rankByUserId["rank"] = el.rank;
       rankByUserId["class"] = el.class;
+      rankByUserId["classImage"] = el.classImage;
       return rankByUserId;
     });
   return result[0];
