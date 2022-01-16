81a4a269078e3365ce4baa6cc4928ce89eba4533
JUYOUNG KIM <wndud2274@gmail.com>
2022-01-16 22:43:01 +0900

    [update] server.js 연결 수정

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const https = require("https");
 const app = require("./app.js");
 
 const { SERVER_PORT, SERVER_DOMAIN } = process.env;
-var server = https.createServer(app);
+var server = http.createServer(app);
 connectDB();
 server.listen(SERVER_PORT, () => {
   console.log(`listening at ${SERVER_DOMAIN}:${SERVER_PORT}`);
