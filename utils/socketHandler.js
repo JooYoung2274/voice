78761a79052309f3dbd9ae8c3d4e5352b427dfa5
JUYOUNG KIM <wndud2274@gmail.com>
2022-01-16 12:24:35 +0900

    [fix] 소켓 인원 카운트 -> 룸 인원 카운트

--- utils/socketHandler.js
+++ utils/socketHandler.js
@@ -55,7 +55,7 @@ io.on("connection", (socket) => {
       if (io.engine.clientsCount === 2) {
         checkChat = true;
       }
-      console.log(io.engine.clientsCount);
+      console.log(io.sockets.adapter.rooms.get(roomNum).size);
       await chatService.createChat({ roomNum, sendUserId, chatText, checkChat });
       sendUserId = await userService.getUserByUserId({ userId: sendUserId });
       const getChat = { sendUserId, receiveUserId, chatText };
