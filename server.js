956addbf37e3de3b5cf30b90c6d0432d89a08718
JUYOUNG KIM <wndud2274@gmail.com>
2022-01-16 22:35:50 +0900

    [update] 채팅 연결 수정

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const { connectDB } = require("./services/index");
 const http = require("http");
+const https = require("https");
 const app = require("./app.js");
 
 const { SERVER_PORT, SERVER_DOMAIN } = process.env;
-
+var server = https.createServer(app);
 connectDB();
 server = app.listen(SERVER_PORT, () => {
   console.log(`listening at ${SERVER_DOMAIN}:${SERVER_PORT}`);
