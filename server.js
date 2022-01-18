f2db89a8dafc4ff08a52a69d68e81b58454b441a
JUYOUNG KIM <wndud2274@gmail.com>
2022-01-18 11:46:58 +0900

    [update] 주석 제거

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const https = require("https");
 const app = require("./app.js");
 
 const { SERVER_PORT, SERVER_DOMAIN } = process.env;
-// var server = http.createServer(app);
+
 connectDB();
 server = app.listen(SERVER_PORT, () => {
   console.log(`listening at ${SERVER_DOMAIN}:${SERVER_PORT}`);
