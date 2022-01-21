00264af7ffe85b34c033ffcda1a818d75da16b17
JUYOUNG KIM <wndud2274@gmail.com>
2022-01-21 11:01:17 +0900

    [fix] 루트 파일 경로 수정

--- utils/converter.js
+++ utils/converter.js
@@ -12,7 +12,7 @@ const s3 = new aws.S3({
 });
 
 const deleteMp3 = (ranFileName) => {
-  const filePath = path.join(`/Users/juyoungkim/Desktop/voice/${ranFileName}`);
+  const filePath = path.join(`/home/ubuntu/voice/${ranFileName}`);
   fs.access(filePath, fs.constants.F_OK, (err) => {
     // A
     if (err) return console.log("삭제할 수 없는 파일입니다");
@@ -23,7 +23,6 @@ const deleteMp3 = (ranFileName) => {
 };
 
 const convertAndSaveS3 = (ranFileName, location) => {
-  console.log(__dirname);
   const key = location.split(".com/")[1];
   let params = { Bucket: S3_BUCKET_NAME, Key: key };
 
