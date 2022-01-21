02214f4b07dc3f066a8a09b7cca6a5d5a0df6c5e
JUYOUNG KIM <wndud2274@gmail.com>
2022-01-21 22:20:00 +0900

    [fix] timeout 수정

--- utils/converter.js
+++ utils/converter.js
@@ -45,7 +45,10 @@ const convertAndSaveS3 = (ranFileName, location) => {
         });
         deleteMp3(ranFileName);
         console.log("업로드!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        resolve();
+        setTimeout(() => {
+          console.log("work!");
+          resolve();
+        }, 500);
       })
       .run();
   });
