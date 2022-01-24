d81d34868ca5595582e9148f9973d6b5e25e3005
doyeon8621 <59997108+doyeon8621@users.noreply.github.com>
2022-01-24 13:23:19 +0900

    [fix]auth router 상수

--- router/auth.js
+++ router/auth.js
@@ -1,32 +1,35 @@
 const express = require("express");
 const passport = require("passport");
-const jwt = require("jsonwebtoken");
-const { needLogin, notNeedLogin } = require("../middleware/auth-middleware");
+const authMiddleware = require("../middleware/auth-middleware");
+const authController = require("../controller/authCtrl");
 const {
-  updateUser,
-  kakaoCallback,
-  googleCallback,
-  naverCallback,
-  getUser,
-  getUserInfo,
-} = require("../controller/authCtrl");
-const { imageMulter } = require("../middleware/uploader");
-const imageUploader = imageMulter.single("profileImage");
+  AUTH_PLATFORM: PLATFORM,
+  AUTH_ROUTE: ROUTE,
+  GOOGLE_SCOPE: SCOPE,
+  PROFILE_UPLOAD: UPLOAD,
+} = require("../config/constants2");
+const uploader = require("../middleware/uploader");
+const imageUploader = uploader.imageMulter.single(UPLOAD.UPLOAD_KEY);
 const router = express.Router();
 
-router.get("/kakao", passport.authenticate("kakao"));
+router.get(ROUTE.KAKAO_LOGIN, passport.authenticate(PLATFORM.KAKAO));
 
-router.get("/kakao/callback", kakaoCallback);
+router.get(ROUTE.KAKAO_CALLBACK, authController.kakaoCallback);
 
-router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
+router.get(ROUTE.GOOGLE_LOGIN, passport.authenticate(PLATFORM.GOOGLE, { scope: [SCOPE.PROFILE] }));
 
-router.get("/google/callback", googleCallback);
+router.get(ROUTE.GOOGLE_CALLBACK, authController.googleCallback);
 
-router.get("/naver", passport.authenticate("naver"));
+router.get(ROUTE.NAVER_LOGIN, passport.authenticate(PLATFORM.NAVER));
 
-router.get("/naver/callback", naverCallback);
+router.get(ROUTE.NAVER_CALLBACK, authController.naverCallback);
 
-router.post("/profile", imageUploader, needLogin, updateUser);
-router.get("/me", needLogin, getUser);
-router.get("/user/:userId", getUserInfo);
+router.post(
+  ROUTE.UPDATE_PROFILE,
+  imageUploader,
+  authMiddleware.needLogin,
+  authController.updateUser,
+);
+router.get(ROUTE.GET_MY_USERINFO, authMiddleware.needLogin, authController.getUser);
+router.get(ROUTE.GET_USERINFO, authController.getUserInfo);
 module.exports = router;
