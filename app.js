4551473dada4fd2490f87c258d607addaf247aa3
JUYOUNG KIM <wndud2274@gmail.com>
2022-01-25 18:14:44 +0900

    [update] cors 수정

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const { reqLimiter } = require("./middleware/security");
 const { DIRECTORY } = require("./config/constants");
 
 const corsOptions = {
-  origin: "*",
+  origin: "https://oao-voice.com",
   credentials: true,
 };
 
@@ -28,7 +28,7 @@ app.use(helmet.frameguard({ action: "deny" })); //iframe 클릭재킹
 app.use(hpp()); //오염된 req.query방어
 
 const { logHandler, errorHandler } = require("./middleware/errorHandler");
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(timeout(DIRECTORY.TIMEOUT));
 
