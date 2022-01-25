72c79c149a1b1e6fbdc19c7c98ad10ca36e16032
JUYOUNG KIM <wndud2274@gmail.com>
2022-01-25 21:39:24 +0900

    [fix] 변수 위치 에러 수정

--- router/auth.js
+++ router/auth.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const passport = require("passport");
 const authMiddleware = require("../middleware/auth-middleware");
 const authController = require("../controller/authCtrl");
-const uploader = require("../middleware/uploader");
-const imageUploader = uploader.imageMulter.single(ETC.UPLOAD_KEY);
 
 const { AUTH_PLATFORM: PLATFORM, ROUTE, ETC } = require("../config/constants");
+
+const uploader = require("../middleware/uploader");
+const imageUploader = uploader.imageMulter.single(ETC.UPLOAD_KEY);
 
 router.get(ROUTE.AUTH.KAKAO, passport.authenticate(PLATFORM.KAKAO));
 router.get(ROUTE.AUTH.KAKAO_CALLBACK, authController.kakaoCallback);
